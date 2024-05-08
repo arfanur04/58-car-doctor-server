b7f34e2f95aea47c4eba53e55cc98be9cfd944d2
arfanur04 <arfanur04@gmail.com>
2024-05-08 20:05:28 +0600

    59-3 Create Checkout Form And Load Single Service Data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -51,6 +51,19 @@ async function run() {
 			res.send(result);
 		});
 
+		app.get("/services/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+
+			const options = {
+				// Include only the `title` and `imdb` fields in the returned document
+				projection: { title: 1, price: 1, service_id: 1 },
+			};
+
+			const result = await serviceCollection.findOne(query, options);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
 		console.log(
@@ -68,7 +81,7 @@ app.get("/", (req, res) => {
 	res.send(`http://${ip}:${port}`); //> remove before deployment
 });
 
-app.listen(port, () => {
+app.listen(port, ip, () => {
 	// console.log(`Server is listening on port ${port}`);
 	console.log(`http://${ip}:${port}`); //> remove before deployment
 });
